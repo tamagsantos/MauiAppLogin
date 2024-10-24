d8b83220609b6bccfd1ff2c43ad13030ce391200
tamagsantos <tamarags.ve@hotmail.com>
2024-10-23 22:23:41 -0300

    Add arquivos ao projeto

--- MauiAppLogin/App.xaml.cs
+++ MauiAppLogin/App.xaml.cs
@@ -8,5 +8,14 @@
 
             MainPage = new AppShell();
         }
-    }
-}
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            return window;
+            window.Width = 100;
+            window.Height = 400;
+            
+        }
+    } //FECHA CLASSE
+} // FECHA NAMESPACE
